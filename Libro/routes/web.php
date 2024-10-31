637e51bcbaf8a937e7ee8d24a9ccf53e17e193b5
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2024-10-31 16:55:14 -0600

    Rutas

--- Libro/routes/web.php
+++ Libro/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\controladorVistas;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/',[controladorVistas::class,'welcome'])->name('welcome');
+
+Route::get('/registro',[controladorVistas::class,'form'])->name('rutaform');
+
+Route::post('/enviarLibro',[controladorVistas::class,'procesarLibro']);
